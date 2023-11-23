528eb11897c11655c7d59e5bf5ee3f77d4e1ca3b
Jamie Sykes <jaymeh@users.noreply.github.com>
2023-11-23 11:34:52 +0000

    Initial Commit.

---
+++ database/seeders/ImageOptimiserDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\ImageOptimiser\database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class ImageOptimiserDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
